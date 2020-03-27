08282ad7aa63ea7817282c2a02c5f69b9b2dda85
eriambrioso <48704694+eriambrioso@users.noreply.github.com>
2020-03-27 10:30:47 -0400

    First Commit
    :)

---
+++ BinarySearch/binary_search.py
@@ -0,0 +1,60 @@
+# Recursive implementation of Binary Search
+
+import random
+from timeit import default_timer as timer
+import matplotlib.pylab as plt
+
+
+def binarySearch(arr, left, right, x):
+    # base case
+    if right >= left:
+        mid = left + (right - left) // 2
+        # element is at the midpoint
+        if arr[mid] == x:
+            return mid
+        # element is in left sub-array
+        elif arr[mid] > x:
+            return binarySearch(arr, left, mid - 1, x)
+        # element is in right sub-array
+        else:
+            return binarySearch(arr, mid + 1, right, x)
+    # element is not found
+    return -1
+
+
+# Driver function
+def main():
+    sizes = [i*5 for i in range(1, 5)]
+    #sizes = [i*1000 for i in range(100)]
+    #print(sizes)
+    timing_vals = dict.fromkeys(sizes, 0)
+    for size in sizes:
+        arr = [i*3 for i in range(size)]
+        size = len(arr)
+        x = random.randint(0, 3*(size-1))
+        #start = timer()
+        loc = binarySearch(arr, 0, size - 1, x)
+        #end = timer()
+
+        print("\nResults on an input size of", size, ':')
+        if loc == -1:
+            print('Element', x, 'not found in list: ')
+            print(arr)
+        else:
+            print('Element', x, 'found at position', loc, 'in list: ')
+            print(arr)
+        """
+        time_elapsed = end - start
+        timing_vals[size] = time_elapsed
+
+   # print(timing_vals)
+    #x, y = zip(*timing_vals)
+
+    plt.plot(list(timing_vals.keys()), list(timing_vals.values()))
+    plt.title("Binary Search time complexity")
+    plt.ylabel("time")
+    plt.xlabel("n")
+    plt.show()
+"""
+
+main()
---
+++ BinarySearch/readme.md
@@ -0,0 +1,11 @@
+# Binary Search in Python
+
+This is a recursive implementation of binary search in python with some profiling data taken. 
+
+Use the command "python binarySeach.py" to run the program
+
+![Sample output](binSearch_test.PNG)
+
+To collect profiling data I used the timeit function from the timer library and plt() from matplotlib to plot my timing data. The expected time complexity is O(log n)
+
+![Timing Data](Figure_1.PNG)
