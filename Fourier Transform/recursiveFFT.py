42953d93cc6171316369b52e6d4fa2283a873776
eriambrioso <48704694+eriambrioso@users.noreply.github.com>
2020-03-27 20:56:00 -0400

    Another commit

--- Fourier Transform/recursiveFFT.py
+++ Fourier Transform/recursiveFFT.py
@@ -1,21 +1,50 @@
+# https://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
+
 import math
+import numpy as np
 
-# Recursive implementation of Fast Fourier Transform
-def FFT(arr):
-    n = len(arr)
-    Wn = exp(2*pi*i/n)
-    w = 1
+def FFT_vectorized(x):
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
 
-    # create 2 new polynomias A0 and A1
-    A0 = [1, 2]
-    A1 = [3, 4]
+    if np.log2(N) % 1 > 0:
+        raise ValueError("size of x must be a power of 2")
 
-# Driver function
+    # N_min here is equivalent to the stopping condition above,
+    # and should be a power of 2
+    N_min = min(N, 32)
+
+    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * np.pi * n * k / N_min)
+    X = np.dot(M, x.reshape((N_min, -1)))
+
+    # build-up each level of the recursive calculation all at once
+    while X.shape[0] < N:
+        X_even = X[:, :X.shape[1] / 2]
+        X_odd = X[:, X.shape[1] / 2:]
+        factor = np.exp(-1j * np.pi * np.arange(X.shape[0])
+                        / X.shape[0])[:, None]
+        X = np.vstack([X_even + factor * X_odd,
+                       X_even - factor * X_odd])
+
+    return X.ravel()
+
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
 def main():
-    arr = [1, 2, 3]
-    FFT(arr)
-
-    print('Results: ')
-
+    x = [0, 1, 2 ,3]
+    #x = np.random.random(32)
+    print('Recursive ', FFT_vectorized(x))
+    print('Iterative ', DFT_slow(x))
 
 main()
